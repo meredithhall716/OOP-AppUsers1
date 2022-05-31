049729522e1bc404a55edbd134cdb51e1854ffb2
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-31 14:39:56 -0400

    first

--- User.py
+++ User.py
@@ -1,1 +1,10 @@
-# your User class goes here
+class User:
+    
+    def __init__(self, name, email_address, drivers_license_number):
+        self.name = name
+        self.email_address = email_address
+        self.drivers_license_number = drivers_license_number
+
+meredith = User('Meredith', 'mhall@gmail.com', 'TX123456')
+jake = User('Jake', 'hilljake@gmail.com', 'MI12345')
+mia = User('Mia', 'mia@mia.com', 'MA123456')
